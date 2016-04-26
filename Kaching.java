a839de1c369167282560ae0b29be9f53c0dd3e28
jhau96 <jhau96@gmail.com>
2016-04-26 12:24:26 -0400

    Added files via upload

---
+++ Kaching.java
@@ -0,0 +1,21 @@
+package edu.mccc.cos210.kc;
+
+import java.awt.image.BufferedImage;
+
+public class Kaching {
+	public Kaching() {	
+	}
+	public static void main(String[] sa) {	
+	}
+	public void saveImage(BufferedImage bi) {	
+	}
+	public void algorithmPriority(BufferedImage bi) {	
+	}
+	public void filePicker() {	
+	}
+	public void startAnimation() {	
+	}
+	public void restart() {	
+	}
+
+}
