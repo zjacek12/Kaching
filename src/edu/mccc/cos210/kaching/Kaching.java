db516897dd5b8ed5d66551a9545ea2c4b3665582
jhau96 <jhau96@gmail.com>
2016-04-27 20:30:39 -0400

    Update Kaching.java

--- src/edu/mccc/cos210/kaching/Kaching.java
+++ src/edu/mccc/cos210/kaching/Kaching.java
@@ -2,11 +2,15 @@ package edu.mccc.cos210.kaching;
 
 //import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.FileDialog;
 import java.awt.image.BufferedImage;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
-public class Kaching {
+public class Kaching extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private FileDialog fd = new FileDialog(this, "Load", FileDialog.LOAD);
 	public Kaching() {	
 		JFrame jf = new JFrame("KACHING");
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,6 +19,9 @@ public class Kaching {
 		jf.setResizable(false);
 		jf.setLocationRelativeTo(null);
 		jf.setVisible(true);
+		JButton jb = new JButton("Load Image");
+		jb.setSize(100, 75);
+		jf.add(jb);
 	}
 	public static void main(String[] sa) {	
 		EventQueue.invokeLater(Kaching::new);
