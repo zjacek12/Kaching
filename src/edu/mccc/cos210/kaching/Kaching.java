37e24275a8a0faa9fa1040f744f3b5cf41636649
jhau96 <jhau96@gmail.com>
2016-04-28 13:45:31 -0400

    Now with semi working buttons

--- src/edu/mccc/cos210/kaching/Kaching.java
+++ src/edu/mccc/cos210/kaching/Kaching.java
@@ -1,27 +1,80 @@
 package edu.mccc.cos210.kaching;
 
+import java.awt.Dimension;
 //import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.FileDialog;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-
-public class Kaching extends JFrame {
+public class Kaching extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
-	private FileDialog fd = new FileDialog(this, "Load", FileDialog.LOAD);
+	private FileDialog fdl = new FileDialog(this, "Load", FileDialog.LOAD);
+	private FileDialog fds = new FileDialog(this, "Save As...", FileDialog.SAVE);
+	private Dimension dimension = new Dimension(100, 75);
+	private FlowLayout layout = new FlowLayout();
 	public Kaching() {	
 		JFrame jf = new JFrame("KACHING");
+		jf.setLayout(layout);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.add(new MyWindow());
 		jf.pack();
-		jf.setResizable(false);
+		jf.setResizable(true);
 		jf.setLocationRelativeTo(null);
 		jf.setVisible(true);
+		fdl.setVisible(false);
+		fds.setVisible(false);
 		JButton jb = new JButton("Load Image");
-		jb.setSize(100, 75);
+		jb.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent ae) {
+						try {
+							fdl.setVisible(true);
+							if (fdl.getFile() != null) {
+							
+							}
+						} catch (Exception e) {
+							System.err.println(e.getMessage());
+							System.exit(-1);
+						}
+					}	
+				}
+			);
+		jb.setPreferredSize(dimension);
 		jf.add(jb);
+		jb = new JButton("Analyze");
+		jb.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent ae) {
+
+					}	
+				}
+			);
+		jb.setPreferredSize(dimension);
+		jf.add(jb);
+		jb = new JButton("Save As...");
+		jb.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					try {
+						fds.setVisible(true);
+						if (fds.getFile() != null) {
+							
+						}
+					} catch (Exception e) {
+						System.err.println(e.getMessage());
+						System.exit(-1);
+					}
+				}	
+			}
+		);
+		jb.setPreferredSize(dimension);
+		jf.add(jb);	
 	}
 	public static void main(String[] sa) {	
 		EventQueue.invokeLater(Kaching::new);
@@ -36,5 +89,7 @@ public class Kaching extends JFrame {
 	}
 	public void restart() {	
 	}
-
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
 }
