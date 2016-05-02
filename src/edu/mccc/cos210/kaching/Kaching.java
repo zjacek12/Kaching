4ae742051e10ab2ad4fa287916cc273f9d00b8b7
jhau96 <jhau96@gmail.com>
2016-05-02 19:47:44 -0400

    Update Kaching.java

--- src/edu/mccc/cos210/kaching/Kaching.java
+++ src/edu/mccc/cos210/kaching/Kaching.java
@@ -1,22 +1,32 @@
 package edu.mccc.cos210.kaching;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Kaching extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private FlowLayout layout = new FlowLayout();
+	private JLabel label;
+	private Dimension dimension = new Dimension(600, 400);
 	public Kaching() {	
 		JFrame jf = new JFrame("KACHING");
 		jf.setLayout(layout);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.add(new MyWindow());
+		MyWindow window = new MyWindow(this);
+		label = new JLabel();
+		label.setHorizontalAlignment(JLabel.CENTER);
+		jf.add(label, BorderLayout.CENTER);
+		jf.add(window);
 		jf.getContentPane().setBackground(Color.BLACK);
 		jf.pack();
-		jf.setResizable(true);
+		jf.setResizable(false);
 		jf.setLocationRelativeTo(null);
 		jf.setVisible(true);
 	}
@@ -28,5 +38,9 @@ public class Kaching extends JFrame {
 	public void startAnimation() {	
 	}
 	public void restart() {	
+	}
+	public void setPicture(ImageIcon icon) {
+		label.setIcon(icon);	
+		label.setMaximumSize(dimension);
 	}
 }
